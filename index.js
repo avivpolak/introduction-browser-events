cedf1483a3092453094b406bead6085dbc3aca80
AVIV POLAK <avivp0437@gmail.com>
2021-09-15 08:27:56 +0300

    hideself

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 "use strict"
+//hide text
 document.getElementById('elem').addEventListener("click", hide) 
  
 function hide () {
     document.getElementById('text').hidden = true;
   }
+
+
+
+//hideself
+function hideself(){
+  this.hidden = true;
+}
+document.getElementById('elemn').addEventListener("click",hideself)
