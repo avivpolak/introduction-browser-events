b32930bc26eb3caaa8ffb59f04865876187012bb
AVIV POLAK <avivp0437@gmail.com>
2021-09-15 13:30:18 +0300

    emoji list scroll left right

--- index.js
+++ index.js
@@ -63,12 +63,8 @@ function openul() {
     document.getElementById("ul").classList.toggle("hide");
 }
 
-
-
-
-//pop paragraph
 let body = document.getElementById("body");
-let outerdiv=createElement("div",[],[],{"id":"outerdiv"})
+let outerdiv = createElement("div", [], [], { id: "outerdiv" });
 body.appendChild(outerdiv);
 for (let i = 0; i < 5; i++) {
     let div = document.createElement("div");
@@ -78,26 +74,68 @@ for (let i = 0; i < 5; i++) {
         "src",
         "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/X_mark.svg/525px-X_mark.svg.png"
     );
-    x.setAttribute("id",i);
+    x.setAttribute("id", i);
     x.style.width = "40px";
     x.style.width = "40px";
-    p.innerText = i+ `The domestic cat (Latin: Felis catus) is a small, typically furry, carnivorous mammal. They are often called house cats when kept as indoor pets or simply cats when there is no need to distinguish them from other felids and felines. Cats are often valued by humans for companionship and for their ability to hunt vermin.`;
+    p.innerText =
+        i +
+        `The domestic cat (Latin: Felis catus) is a small, typically furry, carnivorous mammal. They are often called house cats when kept as indoor pets or simply cats when there is no need to distinguish them from other felids and felines. Cats are often valued by humans for companionship and for their ability to hunt vermin.`;
     let body = document.getElementById("outerdiv");
 
     div.appendChild(x);
     div.appendChild(p);
     body.appendChild(div);
 }
-outerdiv.addEventListener("click", pop)
-function pop (e){
-  const element = e.target;
-  if (element.id)  element.parentElement.classList.toggle("hide")
-
+outerdiv.addEventListener("click", pop);
+function pop(e) {
+    const element = e.target;
+    if (element.id / 1) element.parentElement.classList.toggle("hide");
 }
 
 
 
 
+
+
+
+
+
+
+/* label the images, just for convenience, to visually track them */
+let i = 1;
+for (let li of carousel.querySelectorAll("li")) {
+    li.style.position = "relative";
+    li.insertAdjacentHTML(
+        "beforeend",
+        `<span style="position:absolute;left:0;top:0">${i}</span>`
+    );
+    i++;
+}
+
+/* configuration */
+let width = 130; // image width
+let count = 3; // visible images count
+
+let listy = carousel.querySelector("ul");
+let listElems = carousel.querySelectorAll("li");
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector(".prev").onclick = function () {
+    // shift left
+    position += width * count;
+    // can't move to the left too much, end of images
+    position = Math.min(position, 0);
+    listy.style.marginLeft = position + "px";
+};
+
+carousel.querySelector(".next").onclick = function () {
+    // shift right
+    position -= width * count;
+    // can only shift the ribbbon for (total ribbon length - visible count) images
+    position = Math.max(position, -width * (listElems.length - count));
+    listy.style.marginLeft = position + "px";
+};
 
 
 
@@ -126,32 +164,29 @@ function pop (e){
 
 
 function createElement(tagname, children = [], classes = [], attributes) {
-  //the most generic element builder.we will build all the elements here.
-  const el = document.createElement(tagname)
+    //the most generic element builder.we will build all the elements here.
+    const el = document.createElement(tagname);
 
-  //children
+    //children
 
-  for (let child of children) {
-      if (typeof child === "string" || typeof child === "number") {
-          child = document.createTextNode(child)
-      }
-      el.appendChild(child)
-  }
+    for (let child of children) {
+        if (typeof child === "string" || typeof child === "number") {
+            child = document.createTextNode(child);
+        }
+        el.appendChild(child);
+    }
 
-  //classes
+    //classes
 
-  for (const cls of classes) {
-      el.classList.add(cls)
-  }
+    for (const cls of classes) {
+        el.classList.add(cls);
+    }
 
-  //attrubutes
+    //attrubutes
 
-  for (const attr in attributes) {
-      el.setAttribute(attr, attributes[attr])
-  }
+    for (const attr in attributes) {
+        el.setAttribute(attr, attributes[attr]);
+    }
 
-  return el
+    return el;
 }
-
-
-
