3681d8d28c98928a8bb36e3017a31af7616b3034
AVIV POLAK <avivp0437@gmail.com>
2021-09-15 12:12:52 +0300

    hide list

--- index.js
+++ index.js
@@ -12,7 +12,50 @@ function hide () {
 function hideself(){
   this.hidden = true;
 }
-document.getElementById('elemn').addEventListener("click",hideself
+document.getElementById('elemn').addEventListener("click",hideself)
 
 //which hendler?
 //the answer is line 1 and line 5
+
+//soccer 
+field.onclick = function(event) {
+      let ball = document.getElementById("ball")
+      // window-relative field coordinates
+      let fieldCoords = this.getBoundingClientRect();
+
+      // the ball has position:absolute, the field: position:relative
+      // so ball coordinates are relative to the field inner left-upper corner
+      let ballCoords = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+      };
+
+      // prevent crossing the top field boundary
+      if (ballCoords.top < 0) ballCoords.top = 0;
+
+      // prevent crossing the left field boundary
+      if (ballCoords.left < 0) ballCoords.left = 0;
+
+
+      // // prevent crossing the right field boundary
+      if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+      }
+
+      // prevent crossing the bottom field boundary
+      if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+      }
+
+      ball.style.left = ballCoords.left + 'px';
+      ball.style.top = ballCoords.top + 'px';
+}
+
+//hide list
+let list = document.getElementById("list")
+list.addEventListener("click", openul)
+function openul(){
+  document.getElementById("ul").classList.toggle("hide")
+}
+
+
