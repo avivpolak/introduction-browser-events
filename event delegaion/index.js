6687cdb50a25e58dfcda3c1fa87f769239ca5d89
AVIV POLAK <avivp0437@gmail.com>
2021-09-19 05:50:24 +0300

    tree

--- event delegaion/index.js
+++ event delegaion/index.js
@@ -26,25 +26,16 @@ function createElement(tagname, children = [], classes = [], attributes) {
 
     return el;
 }
-let tree=document.getElementById("tree")
 
-        // move all text into <span>
-        // they occupy exactly the place necessary for the text,
-        for (let li of tree.querySelectorAll('li')) {
-            let span = document.createElement('span');
-            li.prepend(span);
-            span.append(span.nextSibling); // move the text node into span
-          }
-      
-          // catch clicks on whole tree
-          tree.onclick = function(event) {
-      
-            if (event.target.tagName != 'SPAN') {
-              return;
-            }
-      
-            let childrenContainer = event.target.parentNode.querySelector('ul');
-            if (!childrenContainer) return; // no children
-      
-            childrenContainer.hidden = !childrenContainer.hidden;
-          }
+
+document.getElementById("hideChilds").addEventListener("click", hideChilds)
+
+
+function hideChilds(parentElement){
+    for (let child of parentElement){
+        child.style.hidden
+    }
+}
+function handleHide(e){
+    hideChilds(e.target)
+}
